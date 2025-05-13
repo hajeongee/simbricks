4992bcea3214b30eda0788aafdac035e89c21f84
Hejing Li <hajeongee@gmail.com>
2025-05-13 15:50:22 +0000

    fat_tree_fct.py: DC ip configure

--- experiments/pyexps/fat_tree_fct.py
+++ experiments/pyexps/fat_tree_fct.py
@@ -20,36 +20,87 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+import math
 import typing as tp
 import simbricks.orchestration.experiments as exp
 import simbricks.orchestration.nodeconfig as node
 import simbricks.orchestration.simulators as sim
 
-host_pairs = [1]
+host_percentage = [10]
+k_value = 4
+flow_size = 2
+
+# Fat Tree parameters derived from k_value. Not variables
+total_hosts = k_value * k_value * k_value / 4
+num_spine_sw = (k_value / 2) * (k_value / 2)
+num_pod = k_value
+num_agg_sw = k_value / 2
+racks_per_pod = k_value / 2 # num_tor_sw
+num_hosts_per_rack = k_value / 2
 
 experiments = []
 
-for host_p in host_pairs:
-    e = exp.Experiment(f'fat_tree-{host_p}')
+def hostID_to_IP(host_id: int) -> str:
+    """
+    Convert host ID to IP address.
+    Hosts are numbered in a row-major order.
+    The host_0 in the first rack_0 is 0, the host_0 in the rack_1 is 1, and so on.
+
+    """
+    total_num_tor = num_pod * racks_per_pod
+    sub_net_num = int(host_id % total_num_tor)
+    ip1 = 10
+    ip2 = sub_net_num // 256
+    ip3 = sub_net_num % 256
+    ip4 = int(1 + num_agg_sw + (host_id // total_num_tor)) 
+
+    return f'{ip1}.{ip2}.{ip3}.{ip4}'
+
+
+for host_p in host_percentage:
+    e = exp.Experiment(f'FCT_FatTree-{k_value}_{host_p}')
     e.checkpoint = True
 
-    net = sim.SwitchNet()
+    net = sim.NS3FCTNet()
+    net.opt = f'--k_value={k_value} --flow_size={flow_size} --detail_host_percent{host_p/100.0}'
     e.add_network(net)
 
+    # Number of detailed hosts, round up to the nearest even number
+    # For having a pair of server and client
+    num_detail_hosts = round(total_hosts * host_p / 100.0)
+    if num_detail_hosts == 0:
+        num_detail_hosts = 2
+    else:
+        if num_detail_hosts % 2 != 0:
+            num_detail_hosts += 1
+    
+    starting_host_idx = total_hosts - num_detail_hosts
+    last_host_idx = total_hosts - 1
+
+    print(f"detailed_percentage: {host_p}; num_detail_hosts: {num_detail_hosts} from idx {starting_host_idx} - {last_host_idx}")
+
+    for i in range(0, num_detail_hosts):
+        host_id = int(starting_host_idx + i)
+        host_ip = hostID_to_IP(host_id)
+        print(f"host {host_id} ip: {host_ip}")
 
     server_hosts: tp.List[sim.Gem5Host] = []
     client_hosts: tp.List[sim.Gem5Host] = []
 
-    # Create server hosts
-    for i in range(0, host_p):
+    num_pair = num_detail_hosts / 2
+    
+    for i in range(0, num_pair):
+        
+        # Create server hosts
         node_config = node.I40eLinuxNode()
         node_config.prefix = 24
         # node_config.nockp = True
         # server IP starts from 1
-        ip = 1 + i
-        node_config.ip = f'10.0.{int(ip / 256)}.{ip % 256}'
+        server_node_id = int(starting_host_idx + i)
+        server_ip = hostID_to_IP(server_node_id)
+        node_config.ip = server_ip
         node_config.app = node.FCTServer()
-        node_config.force_mac_addr = f'00:90:00:00:00:{ip}'
+        node_config.force_mac_addr = f'00:90:00:00:00:{server_node_id}'
 
         host = sim.Gem5Host(node_config)
         host.cpu_freq = "4GHz"
@@ -63,21 +114,21 @@ for host_p in host_pairs:
 
         e.add_host(host)
 
-        server_hosts.append(host)
+        server_hosts.append(host) 
 
-    # Create client hosts
-    for i in range(0, host_p):
+
+        # Create client hosts
         node_config = node.I40eLinuxNode()
         node_config.prefix = 24
         # node_config.nockp = True
         # client IP starts from 16
-        ip = 1 + host_p + i
-        node_config.ip = f'10.0.{int(ip / 256)}.{ip % 256}'
+        client_node_id = int(last_host_idx - i)
+        client_ip = hostID_to_IP(client_node_id)
+        node_config.ip = client_ip
         node_config.app = node.FCTClient()
-        node_config.force_mac_addr = f'00:90:00:00:00:{ip}'
+        node_config.force_mac_addr = f'00:90:00:00:00:{client_node_id}'
 
-        server_ip = 1 + i
-        node_config.app.server_ip = f'10.0.{int(server_ip / 256)}.{server_ip % 256}'
+        node_config.app.server_ip = server_ip
 
         host = sim.Gem5Host(node_config)
         host.cpu_freq = "4GHz"
@@ -100,3 +151,5 @@ for host_p in host_pairs:
 
     
     experiments.append(e)
+
+
